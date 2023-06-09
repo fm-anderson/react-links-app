744b36fcc3d889642df8ea9d2766dae1feec5c25
AndersonM <andersonmarquesweb@gmail.com>
2023-06-09 16:28:05 -0400

    new component: Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Nav from './components/Nav';
+
 function App() {
   return (
     <div className="container py-4 px-3 mx-auto">
+      <Nav />
       <h1>App</h1>
     </div>
   );
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <div>
+      <h1>Nav</h1>
+    </div>
+  );
+}
+
+export default Nav;
