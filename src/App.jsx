042e142479a7f9de127d1a78eea55b6c729d27c9
AndersonM <andersonmarquesweb@gmail.com>
2023-06-09 16:38:06 -0400

    new pages: Signup, Login, Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Nav from './components/Nav';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
     <div className="container py-4 px-3 mx-auto">
       <Nav />
       <h1>App</h1>
+      <Signup />
+      <Login />
+      <Dashboard />
     </div>
   );
 }
