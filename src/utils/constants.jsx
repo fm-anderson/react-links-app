4121de056a2bb7bb07ac6a4805e7df98beafa3cd
AndersonM <andersonmarquesweb@gmail.com>
2023-06-17 22:14:12 -0400

    fix: routes linked  and authors added

---
+++ src/utils/constants.jsx
@@ -0,0 +1,22 @@
+export const authors = [
+  {
+    name: 'Anderson',
+    link: 'https://github.com/fm-anderson',
+    id: 'fm-anderson',
+  },
+  {
+    name: 'Kirsten',
+    link: 'https://github.com/andkirsten',
+    id: 'andkirsten',
+  },
+  {
+    name: 'Michael',
+    link: 'https://github.com/Michaeljaurigue',
+    id: 'Michaeljaurigue',
+  },
+  {
+    name: 'Tyler',
+    link: 'https://github.com/Wylerlight',
+    id: 'Wylerlight',
+  },
+];
